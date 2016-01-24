278aed52f2d073a8df9d1fc1cdc6e610b745271c
wilkinsleung0712 <wilkinsweiqiangliang@gmail.com>
2016-01-24 17:25:46 +1100

    adding main UI
    adding main UI

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -51,3 +51,15 @@ function createContentPage() {
     );
 	return;
 }
+
+
+// // cleaning controller question answer
+// var pickerDevice = myApp.picker({
+//     input: '#picker-device',
+//     cols: [
+//         {
+//             textAlign: 'center',
+//             values: ['iPhone 4', 'iPhone 4S', 'iPhone 5', 'iPhone 5S', 'iPhone 6', 'iPhone 6 Plus', 'iPad 2', 'iPad Retina', 'iPad Air', 'iPad mini', 'iPad mini 2', 'iPad mini 3']
+//         }
+//     ]
+// });
